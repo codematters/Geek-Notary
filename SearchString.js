3c2d13585d22a1f7d814c50f087003d418bdd19d
codematters <monroe.selin1@gmail.com>
2022-09-12 22:06:07 -0400

    updated ui

--- SearchString.js
+++ SearchString.js
@@ -118,7 +118,7 @@ function filterStopWords(wordArray){
 }
 //a list of stop words we don't want to include in stats
 function getStopWords() {
-    return ["a", "able", "about", "across", "after", "all", "almost","also", "am", "among","i","she","he","the","and","said","to","you","of","my","in","was","that","it","is","as","on","at","her","his","him","me","not","for","be","your","so","but","very","had","have","with","what","this","they","will"]
+    return ["a", "able", "about", "across", "after", "all", "almost", "also", "am", "among", "an", "and", "any", "are", "as", "at", "be", "because", "been", "but", "by", "can", "cannot", "could", "dear", "did", "do", "does", "either", "else", "ever", "every", "for", "from", "get", "got", "had", "has", "have", "he", "her", "hers", "him", "his", "how", "however", "i", "if", "in", "into", "is", "it", "its", "just", "least", "let", "like", "likely", "may", "me", "might", "most", "must", "my", "neither", "no", "nor", "not", "of", "off", "often", "on", "only", "or", "other", "our", "own", "rather", "said", "say", "says", "she", "should", "since", "so", "some", "than", "that", "the", "their", "them", "then", "there", "these", "they", "this", "tis", "to", "too", "twas", "us", "wants", "was", "we", "were", "what", "when", "where", "which", "while", "who", "whom", "why", "will", "with", "would", "yet", "you", "your", "ain't", "aren't", "can't", "could've", "couldn't", "didn't", "doesn't", "don't", "hasn't", "he'd", "he'll", "he's", "how'd", "how'll", "how's", "i'd", "i'll", "i'm", "i've", "isn't", "it's", "might've", "mightn't", "must've", "mustn't", "shan't", "she'd", "she'll", "she's", "should've", "shouldn't", "that'll", "that's", "there's", "they'd", "they'll", "they're", "they've", "wasn't", "we'd", "we'll", "we're", "weren't", "what'd", "what's", "when'd", "when'll", "when's", "where'd", "where'll", "where's", "who'd", "who'll", "who's", "why'd", "why'll", "why's", "won't", "would've", "wouldn't", "you'd", "you'll", "you're", "you've"]
 }
 
 //highlight mark the words in search
@@ -156,3 +156,5 @@ function performMark() {
         element.scrollIntoView();
     };
 }
+
+
